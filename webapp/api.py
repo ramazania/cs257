94993d9cef074c700140dabc9a4e8f4c38eb9358
ramazania <ramazania@LAPTOP-F7UE5P2T.localdomain>
2022-11-09 21:35:34 -0600

    Updated files in webapp

--- webapp/api.py
+++ webapp/api.py
@@ -17,63 +17,52 @@ def get_connection():
                             password=config.password)
 
 
+# @api.route('/')
+# def get_main_page():
+#     return flask.render_template('index.html')
 
-@api.route('/teams[?name={TEAM_NAME}&year={TOURNAMENT_YEAR}]')
-def get_team():
-    return "Hello"
+# @api.route('/teams[?name={TEAM_NAME}&year={TOURNAMENT_YEAR}]')
+# def get_team():
+#     return "Hello"
 
 
-@api.route('/tournament / <year>')
-def get_tournament():
-       ''' Returns a list of all the authors in our database. See
-        get_author_by_id below for description of the author
-        resource representation.
+# @api.route('/tournament / <year>')
+# def get_tournament():
+#     pass
 
-        By default, the list is presented in alphabetical order
-        by last name, then given_name. You may, however, use
-        the GET parameter sort to request sorting by birth year.
 
-            http://.../authors/?sort=birth_year
+# @api.route('/statistics / <year>')
+# def get_statistics():
+#     return "Hello"
 
+@api.route('/tournaments')
+def get_all_tournaments():
+    ''' Returns a list of all the tournament years in our database. 
+        http://.../tournaments
         Returns an empty list if there's any database failure.
     '''
-    query = '''SELECT id, given_name, surname, birth_year, death_year
-               FROM authors ORDER BY '''
+    query = '''SELECT id, tournament_name, year, host_country, winner
+            FROM tournaments '''
 
-    sort_argument = flask.request.args.get('sort')
-    if sort_argument == 'birth_year':
-        query += 'birth_year'
-    else:
-        query += 'surname, given_name'
-
-    author_list = []
+    tournaments_list = []
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        cursor.execute(query, tuple())
+        cursor.execute(query)
         for row in cursor:
-            author = {'id':row[0],
-                      'given_name':row[1],
-                      'surname':row[2],
-                      'birth_year':row[3],
-                      'death_year':row[4]}
-            author_list.append(author)
+            tournament = {'id':row[0],
+                      'tournament_name':row[1],
+                      'year':row[2],
+                      'host_country':row[3],
+                      'winner':row[4]}
+            tournaments_list.append(tournament)
         cursor.close()
         connection.close()
     except Exception as e:
         print(e, file=sys.stderr)
 
-    return json.dumps(author_list)
-
-
-@api.route('/statistics / <year>')
-def get_statistics():
-    return "Hello"
-
-@api.route('/tournaments')
-def get_all_tournaments():
-    return "Hello"
-
-@api.route('/teams')
-def get_all_teams():
-    return "Hello"
+    return json.dumps(tournaments_list)
+    
+# @api.route('/teams')
+# def get_all_teams():
+#     return "Hello"
